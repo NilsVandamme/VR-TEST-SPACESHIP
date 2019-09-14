9d59efdf91023cb66fe79816e1fc3a8788c4bcef
Nils <nils.vandamme98@gmail.com>
2019-09-14 22:25:57 +0200

    Mapping Oculus controls

--- VR Project Spaceship/Assets/Scripts/Player/Controls/TEST_PlayerController.cs
+++ VR Project Spaceship/Assets/Scripts/Player/Controls/TEST_PlayerController.cs
@@ -32,32 +32,32 @@ public class TEST_PlayerController : MonoBehaviour
 
 
         #region rotations controls
-        if (Input.GetKey(KeyCode.Z)) // rotate up
+        if (Input.GetKey(KeyCode.Z) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp)) // rotate up
         {
             rig.AddRelativeTorque(Vector3.left * RotateUpDownSpeed);
         }
 
-        if (Input.GetKey(KeyCode.S)) // rotate down
+        if (Input.GetKey(KeyCode.S) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown)) // rotate down
         {
             rig.AddRelativeTorque(Vector3.right * RotateUpDownSpeed);
         }
 
-        if (Input.GetKey(KeyCode.Q)) // lean left
+        if (Input.GetKey(KeyCode.Q) || OVRInput.Get(OVRInput.Button.Two)) // lean left
         {
             rig.AddRelativeTorque(Vector3.forward * LeanLeftRightSpeed);
         }
 
-        if (Input.GetKey(KeyCode.D)) // lean right
+        if (Input.GetKey(KeyCode.D) || OVRInput.Get(OVRInput.Button.One)) // lean right
         {
             rig.AddRelativeTorque(Vector3.back * LeanLeftRightSpeed);
         }
 
-        if (Input.GetKey(KeyCode.A)) // rotate left
+        if (Input.GetKey(KeyCode.A) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft)) // rotate left
         {
             rig.AddRelativeTorque(Vector3.down * RotateLeftRightSpeed);
         }
 
-        if (Input.GetKey(KeyCode.E)) // rotate right
+        if (Input.GetKey(KeyCode.E) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight)) // rotate right
         {
             rig.AddRelativeTorque(Vector3.up * RotateLeftRightSpeed);
         }
@@ -65,32 +65,32 @@ public class TEST_PlayerController : MonoBehaviour
 
         #region movements controls
 
-        if (Input.GetKey(KeyCode.UpArrow)) // Move forward
+        if (Input.GetKey(KeyCode.UpArrow) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp)) // Move forward
         {
             rig.AddRelativeForce(Vector3.forward * ForwardMoveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow)) // Move backward
+        if (Input.GetKey(KeyCode.DownArrow) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickDown)) // Move backward
         {
             rig.AddRelativeForce(Vector3.back * BackMoveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow)) // Strafe left
+        if (Input.GetKey(KeyCode.LeftArrow) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft)) // Strafe left
         {
             rig.AddRelativeForce(Vector3.left * LeftRightMoveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow)) // Strafe right
+        if (Input.GetKey(KeyCode.RightArrow) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight)) // Strafe right
         {
             rig.AddRelativeForce(Vector3.right * LeftRightMoveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.R)) // Move up
+        if (Input.GetKey(KeyCode.R) || OVRInput.Get(OVRInput.Button.Four)) // Move up
         {
             rig.AddRelativeForce(Vector3.up * UpDownMoveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.F)) // Move down
+        if (Input.GetKey(KeyCode.F) || OVRInput.Get(OVRInput.Button.Three)) // Move down
         {
             rig.AddRelativeForce(Vector3.down * UpDownMoveSpeed);
         }
@@ -99,7 +99,7 @@ public class TEST_PlayerController : MonoBehaviour
 
         #region Reset controls
 
-        if (Input.GetKey(KeyCode.W)) // Smoothly reset rotation
+        if (Input.GetKey(KeyCode.W) || OVRInput.Get(OVRInput.Button.SecondaryThumbstick) ) // Smoothly reset rotation
         {
             rig.rotation = Quaternion.Lerp(rig.rotation, Quaternion.Euler(0, rig.rotation.eulerAngles.y ,0), ResetRotationSpeed);
         }
