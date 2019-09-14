92b070ed6b687d45c7be65361a12baa2ec1919a0
Nils <nils.vandamme98@gmail.com>
2019-09-14 18:36:48 +0200

    Player controller update
    added rotation reset and tweaking values

--- VR Project Spaceship/Assets/Scripts/Player/Controls/TEST_PlayerController.cs
+++ VR Project Spaceship/Assets/Scripts/Player/Controls/TEST_PlayerController.cs
@@ -6,6 +6,17 @@ public class TEST_PlayerController : MonoBehaviour
 {
     private Rigidbody rig;
 
+    [Header("Move Speed")]
+    public float ForwardMoveSpeed;
+    public float BackMoveSpeed;
+    public float LeftRightMoveSpeed;
+
+    [Header("Rotation Speed")]
+    public float RotateUpDownSpeed;
+    public float LeanLeftRightSpeed;
+    public float RotateLeftRightSpeed;
+    public float ResetRotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,55 +33,64 @@ public class TEST_PlayerController : MonoBehaviour
         #region rotations controls
         if (Input.GetKey(KeyCode.A)) // rotate up
         {
-            rig.AddRelativeTorque(Vector3.left);
+            rig.AddRelativeTorque(Vector3.left * RotateUpDownSpeed);
         }
 
         if (Input.GetKey(KeyCode.Q)) // rotate down
         {
-            rig.AddRelativeTorque(Vector3.right);
+            rig.AddRelativeTorque(Vector3.right * RotateUpDownSpeed);
         }
 
         if (Input.GetKey(KeyCode.Z)) // lean left
         {
-            rig.AddRelativeTorque(Vector3.forward * 0.2f);
+            rig.AddRelativeTorque(Vector3.forward * LeanLeftRightSpeed);
         }
 
         if (Input.GetKey(KeyCode.S)) // lean right
         {
-            rig.AddRelativeTorque(Vector3.back * 0.2f);
+            rig.AddRelativeTorque(Vector3.back * LeanLeftRightSpeed);
         }
 
         if (Input.GetKey(KeyCode.E)) // rotate left
         {
-            rig.AddRelativeTorque(Vector3.up);
+            rig.AddRelativeTorque(Vector3.up * RotateLeftRightSpeed);
         }
 
         if (Input.GetKey(KeyCode.D)) // rotate right
         {
-            rig.AddRelativeTorque(Vector3.down);
+            rig.AddRelativeTorque(Vector3.down * RotateLeftRightSpeed);
         }
         #endregion
 
         #region movements controls
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow)) // Move forward
         {
-            rig.AddRelativeForce(Vector3.forward);
+            rig.AddRelativeForce(Vector3.forward * ForwardMoveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow)) // Move backward
         {
-            rig.AddRelativeForce(Vector3.back);
+            rig.AddRelativeForce(Vector3.back * BackMoveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow)) // Strafe left
         {
-            rig.AddRelativeForce(Vector3.left);
+            rig.AddRelativeForce(Vector3.left * LeftRightMoveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow)) // Strafe right
         {
-            rig.AddRelativeForce(Vector3.right);
+            rig.AddRelativeForce(Vector3.right * LeftRightMoveSpeed);
+        }
+
+        #endregion
+
+        #region Reset controls
+
+        if (Input.GetKey(KeyCode.W)) // Smoothly reset rotation
+        {
+            rig.rotation = Quaternion.Lerp(rig.rotation, Quaternion.identity, ResetRotationSpeed);
         }
 
         #endregion
